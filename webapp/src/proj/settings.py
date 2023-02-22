fba9ad8ee0910867545be899c687343faa403bc3
Kalgudin <kalgudin@mail.ru>
2023-02-22 16:07:43 +0300

    its worck

--- webapp/src/proj/settings.py
+++ webapp/src/proj/settings.py
@@ -121,3 +121,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+############################################################################
+
+# cd pycharmprojects\celery-docker
+# venv\scripts\activate
+# python webapp\src\manage.py runserver
+
+# docker-compose up --build
+
